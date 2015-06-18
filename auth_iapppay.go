64372179f4a131014a0c2456a93f2a82d6e04ac4
jixiuf <jixiuf@gmail.com>
2015-06-18 16:00:47 +0800

    iapppay queryresult

--- auth_iapppay.go
+++ auth_iapppay.go
@@ -2,6 +2,7 @@ package goauth
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/0studio/goutils"
 	"net/url"
 	"time"
@@ -35,4 +36,63 @@ func getIAppLoginResponse(v url.Values, now time.Time) (json []byte, err error) 
 type IAppLoginResp struct {
 	Name   string `json:"loginname"`
 	UserId string `json:"userid"`
+}
+
+// 2.4	支付结果查询
+// http://ipay.iapppay.com:9999/payapi/queryresult
+func IPayQueryResult(appid, cporderid, rsaPrivateKey, rsaPublicKey string, now time.Time) (ret IPayQueryResultResponse) {
+
+	reqJsonStruct := iPayQueryResultRequest{
+		AppId:     appid,
+		CPorderid: cporderid,
+	}
+	reqJsonBytes, _ := json.Marshal(reqJsonStruct)
+	transdata := string(reqJsonBytes)
+	reqParams := make(map[string]string)
+	reqParams["transdata"] = transdata
+	reqParams["sign"] = goutils.RSAPack1SignWithMD5(rsaPrivateKey, transdata)
+	reqParams["signtype"] = "RSA"
+	postContent := JoinParam(reqParams, true, true)
+	response, err := goutils.PostHttpResponse("http://ipay.iapppay.com:9999/payapi/queryresult", []byte(postContent), now, DEFAULT_AUTH_HTTP_REQUEST_TIMEOUT)
+	if err != nil {
+		fmt.Println(err)
+		return
+
+	}
+	values, err := url.ParseQuery(string(response))
+	if err != nil {
+		return
+	}
+	data := values.Get("transdata")
+	signRet := values.Get("sign")
+	err = json.Unmarshal([]byte(data), &ret)
+	if err != nil {
+		return
+	}
+	if !goutils.VerifyRSASignWithMD5(rsaPublicKey, data, signRet) {
+		return IPayQueryResultResponse{}
+	}
+	return
+}
+
+type iPayQueryResultRequest struct {
+	AppId     string `json:"appid"`
+	CPorderid string `json:"cporderid"`
+}
+type IPayQueryResultResponse struct {
+	Code      string `json:"code,omitempty"`
+	Result    int32  `json:"result,omitempty"`    // 0–交易成功； 2–待支付
+	CPorderid string `json:"cporderid,omitempty"` // 商户订单号
+	TransId   string `json:"transid,omitempty"`   // 计费支付平台的交易流水号
+	Appuserid string `json:"appuserid,omitempty"` // 用户在商户应用的唯一标识
+	Waresid   string `json:"waresid,omitempty"`   // 平台为应用内需计费商品分配的编码
+	Money     string `json:"money,omitempty"`     // 本次交易的金额
+}
+
+func (res IPayQueryResultResponse) IsOk() bool {
+	if res.Code == "" && res.Result == 0 && res.CPorderid != "" {
+		return true
+	}
+	return false
+
 }
